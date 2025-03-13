62ecdaf617f99cf2bc3f1549a62ede61143902df
Alvaro Jorquera <alvaro.jorquera@safetyclub.cl>
2025-03-12 23:58:13 -0300

    Base

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+    include 'includes/header.php';
+?>
+<div class="hero">
+    <img src="assets/images/background.jpg" alt="Fondo CITECUBB">
+    <div class="overlay">
+        <h1>BIENVENIDOS A CITECUBB</h1>
+        <p>Centro de Investigación en Tecnologías de la Construcción</p>
+    </div>
+</div>
+<?php
+    include 'includes/footer.php';
+?>
